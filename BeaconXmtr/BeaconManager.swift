51c82cf1671fbe52f015a0de52f2026bd752c9fb
Jay Tucker <jtucker@imprivata.com>
2018-05-09 10:19:57 -0400

    Initialize beaconRegion and peripheralManager only once

--- BeaconXmtr/BeaconManager.swift
+++ BeaconXmtr/BeaconManager.swift
@@ -12,33 +12,51 @@ import CoreLocation
 
 class BeaconManager: NSObject {
     
-    fileprivate let proximityUUID = UUID(uuidString: "0C9198B6-417B-4A9C-A5C4-2E2717C6E9C1")!
-    fileprivate let major: CLBeaconMajorValue = 123
-    fileprivate let minor: CLBeaconMinorValue = 456
-    fileprivate let identifier = "com.imprivata.beaconxmtr"
+//    private let proximityUUID = UUID(uuidString: "CE993BE7-C940-45EB-8748-A4E60621CFBA")!
+//    private let identifier = "com.imprivata.imprivataid.shotgunbeacon"
+    
+//    private let proximityUUID = UUID(uuidString: "785FB9F3-C262-4576-BA79-BEBAD30BB278")!
+//    private let identifier = "com.imprivata.imprivataid.laserbeacon"
+    
+    private let proximityUUID = UUID(uuidString: "0C9198B6-417B-4A9C-A5C4-2E2717C6E9C1")!
+    private let identifier = "com.imprivata.beaconxmtr"
 
-    fileprivate var beaconRegion: CLBeaconRegion!
-    fileprivate var peripheralManager: CBPeripheralManager!
+    
+    private let major: CLBeaconMajorValue = 123
+    private let minor: CLBeaconMinorValue = 456
+
+    private var beaconRegion: CLBeaconRegion!
+    private var peripheralManager: CBPeripheralManager!
+    
+    private var isPoweredOn = false
+    
+    override init() {
+        super.init()
+        beaconRegion = CLBeaconRegion(proximityUUID: proximityUUID, identifier: identifier)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
+    }
     
     func startBeacon() {
         print("startBeacon")
-        guard beaconRegion == nil && peripheralManager == nil else {
+        guard isPoweredOn else {
+            print("not powered on")
+            return
+        }
+        guard !peripheralManager.isAdvertising else {
             print("already running")
             return
         }
-        beaconRegion = CLBeaconRegion(proximityUUID: proximityUUID, major: major, minor: minor, identifier: identifier)
-        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
+        let beaconPeripheralData = (beaconRegion.peripheralData(withMeasuredPower: nil) as NSDictionary) as! [String:AnyObject]
+        peripheralManager.startAdvertising(beaconPeripheralData)
     }
 
     func stopBeacon() {
         print("stopBeacon")
-        guard beaconRegion != nil && peripheralManager != nil else {
+        guard peripheralManager.isAdvertising else {
             print("already stopped")
             return
         }
         peripheralManager.stopAdvertising()
-        beaconRegion = nil
-        peripheralManager = nil
     }
 
 }
@@ -48,13 +66,12 @@ extension BeaconManager: CBPeripheralManagerDelegate {
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         print("peripheralManagerDidUpdateState")
         if peripheral.state == .poweredOn {
-            print("PoweredOn")
-            let beaconPeripheralData = (beaconRegion.peripheralData(withMeasuredPower: nil) as NSDictionary) as! [String:AnyObject]
-            peripheralManager.startAdvertising(beaconPeripheralData)
+            print("poweredOn")
         } else if peripheral.state == .poweredOff {
-            print("PoweredOff")
+            print("poweredOff")
             peripheralManager.stopAdvertising()
         }
+        isPoweredOn = peripheral.state == .poweredOn
     }
     
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
